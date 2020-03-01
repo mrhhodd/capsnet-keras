5f2aee22d44101218df2e9b24650e288a4bf4b19
mrhhodd <mrhhodd@gmail.com>
2020-03-01 20:54:08 +0100

    small fixes to train method

--- capsulenet.py
+++ capsulenet.py
@@ -60,6 +60,7 @@ class CapsNet():
 
 
 def train(network, data_gen, save_dir, epochs=30):
+    os.makedirs(save_dir, exist_ok=True)
     network.model.fit(data_gen.training_generator,
               epochs=epochs,
               #   validation_data=data_gen.validation_generator,
@@ -71,5 +72,22 @@ def train(network, data_gen, save_dir, epochs=30):
               ]
               )
 
-    network.model.save_weights(f"{save_dir}/trained_model.h5")
-    print(f"Logs available at {save_dir}")
+    _log_results(cn.model, save_dir, data_gen)
+
+
+def _log_results(model, log_dir, data_gen):
+    # save model summary to file
+    with open(log_dir/"model_summary.txt", "w") as f:
+        with redirect_stdout(f):
+            model.summary()
+
+    # evaluate model and save results to file
+    with open(log_dir/"evaluate.txt", "w") as f:
+        with redirect_stdout(f):
+            print(f"{model.name} result on a test set:")
+            model.evaluate(data_gen.test_generator)
+
+    # save weights to file
+    model.save_weights(str(log_dir/"trained_model.h5"))
+
+    print(f"All logs available in:\n {log_dir}")
