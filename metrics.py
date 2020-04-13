8ecd35d1250a69ee32aa3173f177ebe8448f5702
mrhhodd <mrhhodd@gmail.com>
2020-04-13 17:13:36 +0200

    fix

--- metrics.py
+++ metrics.py
@@ -1,27 +1,31 @@
-
+import tensorflow as tf
 from tensorflow.keras import backend as K
+
+EPSILON = K.epsilon()
 
 def specificity(y_true, y_pred):
     [_, tn, fp, _] = _analyse_data(y_true, y_pred)
-    return tn / (tn + fp + K.epsilon())
+    return K.mean(tn / (tn + fp + EPSILON))
 
 
 def sensitivity(y_true, y_pred):
     [tp, _, _, fn] = _analyse_data(y_true, y_pred)
-    return tp / (tp + fn + K.epsilon())    
+    return K.mean(tp / (tp + fn + EPSILON))   
 
 
 def f1_score(y_true, y_pred):
     [tp, tn, fp, fn] = _analyse_data(y_true, y_pred)
-    precision = tp / (tp + fp + K.epsilon())
-    recall = tp / (tp + fn + K.epsilon())
-    return 2 * precision * recall / (precision + recall + K.epsilon())
+    precision = tp / (tp + fp + EPSILON)
+    recall = tp / (tp + fn + EPSILON)
+    return K.mean(2 * precision * recall / (precision + recall + EPSILON))
 
 
 def _analyse_data(y_true, y_pred):
-    y_pred = K.argmax(y_pred)
-    true_positive = y_true * y_pred
-    true_negative = (1 - y_true) * (1 - y_pred)
-    false_positive = (1 - y_true) * y_pred
-    false_negative = y_true * (1 - y_pred)
+    n_class = y_pred.shape[1]
+    y_pred = K.one_hot(K.argmax(y_pred, axis=1), n_class)
+    true_positive = K.sum(y_true * y_pred, axis=1)
+    true_negative = K.sum((1 - y_true) * (1 - y_pred), axis=1)
+    false_positive = K.sum((1 - y_true) * y_pred, axis=1)
+    false_negative = K.sum(y_true * (1 - y_pred), axis=1)
+    tf.print(true_positive, true_negative, false_positive, false_negative)
     return true_positive, true_negative, false_positive, false_negative
