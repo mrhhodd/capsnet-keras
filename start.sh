976b85900366c4152a376791d3e3845bfafbad65
mrhhodd <mrhhodd@gmail.com>
2020-06-14 19:23:19 +0200

    fix for starting script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 ## Liczba alokowanych węzłów
 #SBATCH -N 1
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=24
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
 #SBATCH --mem-per-cpu=10GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
