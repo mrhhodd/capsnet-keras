7fcf8d1074368ac92ff6f20f4f7f66cce37057e8
mrhhodd <mrhhodd@gmail.com>
2020-06-14 18:48:36 +0200

    fix for starting script

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ from capsulenet import CapsNet, train
 from data_generators import DataGen
 
 
-INPUT_SHAPE = (128,128,1)
+SHAPE = int(os.getenv('SHAPE', 128))
+INPUT_SHAPE = (SHAPE, SHAPE, 1)
 DATA_DIR = os.getenv('DATA_DIR')
 RESULTS_BASE_DIR = os.getenv('RESULTS_BASE_DIR')
 EPOCHS = os.getenv('EPOCHS')
 BATCH_SIZE = os.getenv('BATCH_SIZE')
-CLASSES = 4
 
 
 if __name__ == "__main__":
-    cn = CapsNet(n_class=CLASSES, input_shape=INPUT_SHAPE, batch_size=BATCH_SIZE)
+    cn = CapsNet(n_class=4, input_shape=INPUT_SHAPE, batch_size=BATCH_SIZE)
 
     data_gen = DataGen(
         batch_size=BATCH_SIZE, 
--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH -p plgrid-gpu-v100
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --time 1:00:00
 
-DATA_DIR=/people/plgmwnetrzak/data/124x124_OCT2017_BAL \
-RESULTS_BASE_DIR=/people/plgmwnetrzak/result/124x124_OCT2017_BAL \
+export LD_LIBRARY_PATH=/net/people/plgmwnetrzak/magisterka/cuda:$LD_LIBRARY_PATH 
+
+SHAPE=124 \
+DATA_DIR=/people/plgmwnetrzak/data/124x124_OCT2017 \
+RESULTS_BASE_DIR=/people/plgmwnetrzak/result/124x124_OCT2017 \
 EPOCHS=2 BATCH_SIZE=16 \
 python /people/plgmwnetrzak/capsnet_keras/main.py
