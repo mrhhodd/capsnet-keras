92e59395d74c156add777fa5e2872cc591e89639
mrhhodd <mrhhodd@gmail.com>
2020-06-21 17:41:37 +0200

    change beta_a initialization

--- layers.py
+++ layers.py
@@ -91,7 +91,7 @@ class BaseCaps(layers.Layer):
             name='beta_a',
             shape=[self.capsules],
             # initializer='glorot_uniform',
-            initializer=initializers.TruncatedNormal(mean=-1000.0, stddev=500.0),
+            initializer=initializers.TruncatedNormal(mean=-2000.0, stddev=500.0),
             regularizer=self.weights_regularizer,
             trainable=True)
 
