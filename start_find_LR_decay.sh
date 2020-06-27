e95a3f629e0320d58316d5dd4d194b665e32c36f
mrhhodd <mrhhodd@gmail.com>
2020-06-27 17:44:45 +0200

    add more hiperparameter scripts

--- start_find_LR_decay.sh
+++ start_find_LR_decay.sh
@@ -23,7 +23,7 @@ for LR_DECAY in 1 0.999 0.995 0.99 0.985 0.98 0.975 0.97 0.965 0.96 0.955 0.95 0
 do
     SHAPE=128 \
     DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/2 \
-    RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/OCT2017_128x128_SBB/LR_TEST_$LR/ \
+    RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/OCT2017_128x128_SBB/LR_DECAY_TEST_$LR_DECAY/ \
     EPOCHS=20 BATCH_SIZE=96 ROUTINGS=3 LR=0.05 LR_DECAY=$LR_DECAY \
     python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/main.py
 done
