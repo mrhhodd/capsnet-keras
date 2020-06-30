ed0d6a4bfe8e8e57e81f7818b180f763d0f311f5
mrhhodd <mrhhodd@gmail.com>
2020-06-30 08:59:16 +0200

    temporary enable image normalization

--- data_generators.py
+++ data_generators.py
@@ -4,6 +4,7 @@ from pathlib import Path
 class DataGen():
     def __init__(self, batch_size, data_dir, target_size, validation_split):
         data_gen = ImageDataGenerator(
+            preprocessing_function=lambda input: (input - 127.0)/128.0,
             validation_split=validation_split
         )
         self.training_generator = data_gen.flow_from_directory(
