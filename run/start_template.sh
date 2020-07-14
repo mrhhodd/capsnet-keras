b196f37ffeccc2518e9713cb88745e50af672ec7
mrhhodd <mrhhodd@gmail.com>
2020-07-14 19:59:58 +0200

    add some start scripts

--- run/start_template.sh
+++ run/start_template.sh
@@ -27,6 +27,6 @@ ROUTINGS=3 \
 LR=0.04 \
 LR_DECAY=0.97 \
 RR=0.0000002 \
-A=64, B=8,C=16,D=16, cckernel=5,
+A=64, B=8,C=16,D=16, cckernel=5 \
 RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/OCT2017_128x128_SBB/${EPOCHS}EPOCHS_${ROUTINGS}ROUTINGS_${LR}LR_${LR_DECAY}LR_DECAY_${RR}RR_${A}A_${B}B_${C}C_${D}D_${cckernel}cckernel/ \
 python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/main.py
