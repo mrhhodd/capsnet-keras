36739efec89332ba6429859463ca25edbbf363c1
mrhhodd <mrhhodd@gmail.com>
2020-07-19 18:27:58 +0200

    try to find best LR

--- run/start_find_LR.sh
+++ run/start_find_LR.sh
@@ -4,15 +4,15 @@
 ## Liczba alokowanych węzłów
 #SBATCH -N 1
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=8
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
 #SBATCH --mem-per-cpu=5GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=18:00:00 
+#SBATCH --time=12:00:00 
 ## Nazwa grantu do rozliczenia zużycia zasobów
 #SBATCH -A plgcapsnet
 #SBATCH --partition=plgrid-gpu-v100
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 
 
 export LD_LIBRARY_PATH=/net/people/plgmwnetrzak/magisterka/cuda/lib64:$LD_LIBRARY_PATH 
