038baaac129812a87a4af6888803cca57bad1096
mrhhodd <mrhhodd@gmail.com>
2020-07-23 20:58:20 +0200

    code cleanup

--- run.py
+++ run.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
 
     if WEIGHTS:
         print("Loading and evaluating model")
-        cn.load_weights(weights_file=WEIGHTS)
+        cn.load_weights(weights_file=str(WEIGHTS))
         test(
             model=cn.model,
             data_gen=data_gen,
