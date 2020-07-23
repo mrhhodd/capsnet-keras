798a675afbee7ae4342cf3f4140eb235fdb8839b
mrhhodd <mrhhodd@gmail.com>
2020-07-23 20:52:17 +0200

    code cleanup

--- run.py
+++ run.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
         if USE_LR_DECAY:
             cn_callbacks.append(
                 callbacks.LearningRateScheduler(
-                    schedule=lambda epoch, lr: lr * LR_DECAY ** K.minimum(20000.0, epoch))
+                    schedule=lambda epoch, lr: lr * LR_DECAY ** min(20000.0, epoch))
             )
 
         train(
