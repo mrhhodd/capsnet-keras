0aa8905514b1aebea2c566511d4600d12b5ed82d
mrhhodd <mrhhodd@gmail.com>
2020-07-23 19:31:37 +0200

    add scripts for a final run

--- run/final_run.sh
+++ run/final_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 for split in 0.2 0.3 0.4; do
     for ((i=0;i<=9;i++)); do
-        I=${i} SPLIT=${split} /net/people/plgmwnetrzak/magisterka/capsnet-keras/run/final_single_run.sh
+        I=${i} SPLIT=${split} sbatch /net/people/plgmwnetrzak/magisterka/capsnet-keras/run/final_single_run.sh
     done
 done
