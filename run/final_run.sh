d830672eefa40ed18839185e014b89a8ea79e21f
mrhhodd <mrhhodd@gmail.com>
2020-07-23 19:30:59 +0200

    try to find best LR

--- run/final_run.sh
+++ run/final_run.sh
@@ -4,18 +4,3 @@ for split in 0.2 0.3 0.4; do
         I=${i} SPLIT=${split} /net/people/plgmwnetrzak/magisterka/capsnet-keras/run/final_single_run.sh
     done
 done
-# SHAPE=128 \
-# DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/${split}/${i} \
-# EPOCHS=30 \
-# VALIDATION_SPLIT=${split} \
-# BATCH_SIZE=12 \
-# ROUTINGS=2 \
-# LR=0.04 \
-# LR_DECAY=0.97 \
-# USE_LR_DECAY=True \
-# RR=0.0000002 \
-# A=96 B=12 C=16 D=16 \
-# RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/finals/ \
-# python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/run.py
-
-
--- run/final_single_run.sh
+++ run/final_single_run.sh
@@ -15,23 +15,22 @@
 #SBATCH --gres=gpu:4
 
 
-# export LD_LIBRARY_PATH=/net/people/plgmwnetrzak/magisterka/cuda/lib64:$LD_LIBRARY_PATH 
-# export PYTHONPATH=/net/people/plgmwnetrzak/magisterka/capsnet-keras:$PYTHONPATH 
-# module add plgrid/libs/tensorflow-gpu/2.2.0-python-3.8 
+export LD_LIBRARY_PATH=/net/people/plgmwnetrzak/magisterka/cuda/lib64:$LD_LIBRARY_PATH 
+export PYTHONPATH=/net/people/plgmwnetrzak/magisterka/capsnet-keras:$PYTHONPATH 
+module add plgrid/libs/tensorflow-gpu/2.2.0-python-3.8 
 
-echo "${SPLIT} ${I}" 
-# SHAPE=128 \
-# DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/${split}/${i} \
-# EPOCHS=30 \
-# VALIDATION_SPLIT=${split} \
-# BATCH_SIZE=12 \
-# ROUTINGS=2 \
-# LR=0.04 \
-# LR_DECAY=0.97 \
-# USE_LR_DECAY=True \
-# RR=0.0000002 \
-# A=96 B=12 C=16 D=16 \
-# RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/finals/ \
-# python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/run.py
+SHAPE=128 \
+DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/${split}/${i} \
+EPOCHS=30 \
+VALIDATION_SPLIT=${split} \
+BATCH_SIZE=12 \
+ROUTINGS=2 \
+LR=0.04 \
+LR_DECAY=0.97 \
+USE_LR_DECAY=True \
+RR=0.0000002 \
+A=96 B=12 C=16 D=16 \
+RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/finals/ \
+python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/run.py
 
 
