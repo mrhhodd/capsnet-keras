5b441381b4a08a02449e356d3413502ea1171a71
mrhhodd <mrhhodd@gmail.com>
2020-07-25 09:08:07 +0200

    try different beta values

--- run/final_single_run.sh
+++ run/final_single_run.sh
@@ -4,7 +4,7 @@
 ## Liczba alokowanych węzłów
 #SBATCH -N 1
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=8
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
 #SBATCH --mem-per-cpu=5GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
@@ -12,15 +12,17 @@
 ## Nazwa grantu do rozliczenia zużycia zasobów
 #SBATCH -A plgcapsnet
 #SBATCH --partition=plgrid-gpu-v100
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 
+i=$1
+split=$2
 
 export LD_LIBRARY_PATH=/net/people/plgmwnetrzak/magisterka/cuda/lib64:$LD_LIBRARY_PATH 
 export PYTHONPATH=/net/people/plgmwnetrzak/magisterka/capsnet-keras:$PYTHONPATH 
 module add plgrid/libs/tensorflow-gpu/2.2.0-python-3.8 
 
 SHAPE=128 \
-DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/${split}/${i} \
+DATA_DIR=/net/people/plgmwnetrzak/magisterka/data/OCT2017_128x128_SBB/${i} \
 EPOCHS=30 \
 VALIDATION_SPLIT=${split} \
 BATCH_SIZE=12 \
@@ -30,7 +32,7 @@ LR_DECAY=0.97 \
 USE_LR_DECAY=True \
 RR=0.0000002 \
 A=96 B=12 C=16 D=16 \
-RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/finals/ \
+RESULTS_BASE_DIR=/net/people/plgmwnetrzak/magisterka/result/finals/${split}/${i} \
 python3 /net/people/plgmwnetrzak/magisterka/capsnet-keras/run.py
 
 
