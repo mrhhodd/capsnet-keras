c23478e66ef13c8241f053dac5531830c0898f0b
mrhhodd <mrhhodd@gmail.com>
2020-07-30 21:02:42 +0200

    add cnn baseline

--- run/final_run_cnn.sh
+++ run/final_run_cnn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 for split in 0.2 0.3 0.4; do
     for ((i=0;i<=9;i++)); do
-        sbatch /net/people/plgmwnetrzak/magisterka/capsnet-keras/run/final_single_run.sh ${i} ${split}
+        sbatch /net/people/plgmwnetrzak/magisterka/capsnet-keras/run/final_single_run_cnn.sh ${i} ${split}
     done
 done
